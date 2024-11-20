ed9f94069c71d0e8138a38745e32a83254fa2e74
Tyler <tbos92@gmail.com>
2024-11-19 20:12:10 -0500

    Finalized db coding, curl command tests all worked!

---
+++ server/db.js
@@ -0,0 +1,109 @@
+require("dotenv").config();
+
+const pg = require("pg");
+const client = new pg.Client(process.env.DATABASE_URL);
+
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
+
+const createTables = async () => {
+  const SQL = /*sql */ `
+    DROP TABLE IF EXISTS favorites;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS products;
+    CREATE TABLE users(
+        id UUID PRIMARY KEY,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        password VARCHAR(255)
+    );
+    CREATE TABLE products(
+        id UUID PRIMARY KEY,
+        name VARCHAR(50) NOT NULL
+    );
+    CREATE TABLE favorites(
+        id UUID PRIMARY KEY,
+        user_id UUID REFERENCES users(id) NOT NULL,
+        product_id UUID REFERENCES products(id) NOT NULL
+    );
+    `;
+
+  try {
+    await client.query(SQL);
+    console.log("Tables created successfully.");
+  } catch (error) {
+    console.error("Error creating tables:", error);
+  }
+};
+
+const createUser = async ({ username, password }) => {
+  const SQL = /*sql */ `
+  INSERT INTO users (id, username, password) VALUES($1, $2, $3) RETURNING *
+  `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    username,
+    await bcrypt.hash(password, 5),
+  ]);
+  return response.rows[0];
+};
+
+const createProduct = async ({ name }) => {
+  const SQL = /*sql */ `
+    INSERT INTO products(id, name) VALUES($1, $2) RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+
+const fetchUsers = async () => {
+  const SQL = /*sql */ `
+  SELECT * FROM users
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchProducts = async () => {
+  const SQL = /*sql */ `
+    SELECT * FROM products
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const createFavorite = async ({ product_id, user_id }) => {
+  const SQL = /*sql */ `
+    INSERT INTO favorites(id, product_id, user_id) VALUES($1, $2, $3) RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid.v4(), product_id, user_id]);
+  return response.rows[0];
+};
+
+const fetchFavorites = async () => {
+  const SQL = /*sql */ `
+      SELECT * FROM favorites
+      `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const destroyFavorite = async ({ id, user_id }) => {
+  console.log(id, user_id);
+  const SQL = /*sql */ `
+  DELETE FROM favorites
+  WHERE id = $1 AND user_id = $2
+  `;
+  await client.query(SQL, [id, user_id]);
+};
+
+module.exports = {
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  fetchUsers,
+  fetchProducts,
+  createFavorite,
+  fetchFavorites,
+  destroyFavorite,
+};
---
+++ server/index.js
@@ -0,0 +1,134 @@
+require("dotenv").config();
+
+const {
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  fetchUsers,
+  fetchProducts,
+  createFavorite,
+  fetchFavorites,
+  destroyFavorite,
+} = require("./db");
+
+const express = require("express");
+const app = express();
+
+app.use(require("morgan")("dev"));
+app.use(express.json());
+
+// READ users using GET /api/users
+app.get("/api/users", async (req, res, next) => {
+  try {
+    res.send(await fetchUsers());
+  } catch (error) {
+    next(error);
+  }
+});
+
+// READ products using GET /api/products
+app.get("/api/products", async (req, res, next) => {
+  try {
+    res.send(await fetchProducts());
+  } catch (error) {
+    next(error);
+  }
+});
+
+// READ favorites using GET /api/favoritesa
+app.get("/api/favorites", async (req, res, next) => {
+  try {
+    res.send(await fetchFavorites());
+  } catch (error) {
+    next(error);
+  }
+});
+
+// REMOVE specific favorite using DELETE /api/users/:user_id/favorites/:id
+app.delete("/api/users/:user_id/favorites/:id", async (req, res, next) => {
+  try {
+    await destroyFavorite({
+      user_id: req.params.user_id,
+      id: req.params.id,
+    });
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST /api/users/:user_id/favorites - Returns a created favorite with a user and product
+app.post("/api/users/:user_id/favorites", async (req, res, next) => {
+  try {
+    res.status(201).send(
+      await createFavorite({
+        user_id: req.params.user_id,
+        product_id: req.body.product_id,
+      })
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+
+const init = async () => {
+  await client.connect();
+  await createTables();
+
+  const [
+    alice,
+    brian,
+    catherine,
+    david,
+    evelyn,
+    earbuds,
+    officeChair,
+    phoneStand,
+  ] = await Promise.all([
+    createUser({ username: "AJohnson", password: "securepassword" }),
+    createUser({ username: "BSmith", password: "1234" }),
+    createUser({ username: "CLee", password: "superdupersafe" }),
+    createUser({ username: "DKim", password: "dfsdfgdgg" }),
+    createUser({ username: "EMartinez", password: "unicorn-purple-potato" }),
+    createProduct({ name: "Wireless Earbuds" }),
+    createProduct({ name: "Ergonomic Office Chair" }),
+    createProduct({ name: "Smartphone Stand" }),
+  ]);
+  console.log(await fetchUsers());
+  console.log(await fetchProducts());
+
+  const [favorite, favorite2] = await Promise.all([
+    createFavorite({
+      user_id: alice.id,
+      product_id: earbuds.id,
+    }),
+    createFavorite({
+      user_id: david.id,
+      product_id: phoneStand.id,
+    }),
+  ]);
+  console.log(await fetchFavorites());
+
+  await destroyFavorite({
+    id: favorite.id,
+    user_id: favorite.user_id,
+  });
+  console.log(await fetchFavorites());
+
+  const port = process.env.PORT;
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+
+    console.log(`curl localhost:${port}/api/users`);
+    console.log(`curl localhost:${port}/api/products`);
+    console.log(`curl localhost:${port}/api/favorites`);
+    console.log(
+      `curl -X DELETE localhost:${port}/api/users/${alice.id}/favorites/${favorite2.id}`
+    );
+    console.log(
+      `curl -X POST localhost:${port}/api/users/${evelyn.id}/favorites -d '{"user_id":"${evelyn.id}", "product_id":"${officeChair.id}"}' -H "Content-Type:application/json"`
+    );
+  });
+};
+init();
